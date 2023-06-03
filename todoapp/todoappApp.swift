627f6155e14c87c0149ecc3402d54f7ff6fd9149
Sanjida Orpi <so2189@nyu.edu>
2023-06-03 16:23:18 -0400

    Initial Commit

---
+++ todoapp/todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappApp.swift
+//  todoapp
+//
+//  Created by Sanjida Orpi on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
